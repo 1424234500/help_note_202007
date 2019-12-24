65ced54582df4cd0c316af4c7335774a2bfc481d
Walker <1424234500@qq.com>
2019-12-24 20:30:02 +0800

    2019年 12月 24日 星期二 20:30:02 CST

--- cmd/help_cmd.c
+++ cmd/help_cmd.c
@@ -220,33 +220,8 @@ dir /?
 cd /home 进入 '/ home' 目录' 
 　　cd .. 返回上一级目录 
 　　cd ../.. 返回上两级目录 
-　　cd 进入个人的主目录 
-　　cd ~ 进入个人的主目录 
-　　cd - 返回上次所在的目录 
-　　pwd 显示工作路径 
-　　ls 查看目录中的文件 
-　　ls -F 查看目录中的文件 
-　　ls -l 显示文件和目录的详细资料 
-　　ls -a 显示隐藏文件 
-　　ls *[0-9]* 显示包含数字的文件名和目录名 
-　　tree 显示文件和目录由根目录开始的树形结构(1) 
-　　lstree 显示文件和目录由根目录开始的树形结构(2) 
-　　mkdir dir1 创建一个叫做 'dir1' 的目录' 
-　　mkdir dir1 dir2 同时创建两个目录 
-　　mkdir -p /tmp/dir1/dir2 创建一个目录树 
-　　rm -f file1 删除一个叫做 'file1' 的文件' 
-　　rmdir dir1 删除一个叫做 'dir1' 的目录' 
-　　rm -rf dir1 删除一个叫做 'dir1' 的目录并同时删除其内容 
-　　rm -rf dir1 dir2 同时删除两个目录及它们的内容 
-　　mv dir1 new_dir 重命名/移动 一个目录 
-　　cp file1 file2 复制一个文件 
-　　cp dir/* . 复制一个目录下的所有文件到当前工作目录  */
-　　cp -a /tmp/dir1 . 复制一个目录到当前工作目录 
-　　cp -a dir1 dir2 复制一个目录 
-　　ln -s file1 lnk1 创建一个指向文件或目录的软链接 
-　　ln file1 lnk1 创建一个指向文件或目录的物理链接 
-　　touch -t 0712250000 file1 修改一个文件或目录的时间戳 - (YYMMDDhhmm)
-文件搜索
+　　 
+文件搜索grep ?
 find /?
     文件目录检索批量 文本关键词
     find /I /N "keys" *
